fe0fc9cc6b1ff9a9d8fed77df834c86788acc8ff
Kazumi566 <barganmihai.com@gmail.com>
2024-05-24 04:08:49 +0300

    Added backend code

---
+++ movies/src/main/java/com/example/movies/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.movies;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**")
+                        .allowedOrigins("http://localhost:3000", "https://e1bd-109-185-76-100.ngrok-free.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
